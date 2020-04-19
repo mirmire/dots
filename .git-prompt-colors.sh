26529b16a4c7ee9e2961c89783c103bcc3a3b1d3
Prakash Acharya <33595598+mirmire@users.noreply.github.com>
2020-04-19 09:31:44 +0300

    git-prompt-colors: show nothing if the repo is clean
    much easier to understand -- clean shows nothing -- as well as
    intuitive.

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -51,7 +51,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_REMOTE=" "
   GIT_PROMPT_UNTRACKED=" ${Cyan}…"
   GIT_PROMPT_STASHED=" ${Cyan}⚑"
-  GIT_PROMPT_CLEAN=" ${Green}❯❯"
+  GIT_PROMPT_CLEAN=""
   GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING=""
 
   GIT_PROMPT_COMMAND_OK="${Green}✔"
